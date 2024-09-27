305ec6e7a47ff99486bb260c23d8e12004a2c33c
bhagyarucha10 <bhagyashree.mohan@gmail.com>
2024-09-27 10:45:55 +0530

    first change

---
+++ SpringJPAQuery/src/test/java/com/example/SpringJPAQuery/SpringJpaQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJPAQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
